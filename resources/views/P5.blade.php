392f2f9220dde03ec6b711d7274d8e8ebe2de0de
fajar288 <fajrzprayoga@gmail.com>
2025-04-22 11:25:05 +0700

    Update P5

---
+++ resources/views/P5.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link href="styles/style.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <div>
+        <img src="images/Gambar1.png" alt="1"/>
+        <img src="images/Gambar2.png" alt="2"/>
+    </div>
+</body>
+</html>
+
